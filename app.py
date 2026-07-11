014db453a8e385a313883161bbbd46fbae79e0fa
25s189-cpu <25s189@yongsan.sen.hs.kr>
2026-07-11 15:11:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import streamlit as st
+
+st.markdown("# 앱 UI 만들기")
+st.header("이름")
+이름 = st.text_input(" ", placeholder="name_on_here")
+
+st.header("학년)
+학년 = st.radio(" ", [1, 2, 3], hrizontal=True)
+
+st.header("반")
+반 = st.slectbox(" ", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+st.header("난이도")
+난이도 = st.slect_slider(" ", options=["매우 쉬움", "쉬움", "보통", "어려움", "매우 어려움"])
+
+st.header("점수")
+점수 = st.slider(" ", 0, 100, 1)
+
+st.header("소감")
+소감 = st.text_area(" ", placeholder="소감입니다.")
+
+if st.button("확인"):
+  st.markdown(f"{이름}/{학년}학년/{반}반/{난이도}")
+  st.markdown(f"점수: {점수}")
+  st.markdown(f"소감: {소감}}")
+
+
+
+
 
